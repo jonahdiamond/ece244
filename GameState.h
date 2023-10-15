b086d077e9bdca2d42313e13fcd4b23424538020
Jonah Diamond <132615526+jonahdiamond@users.noreply.github.com>
2023-10-14 22:01:04 -0400

    Update GameState.h

--- GameState.h
+++ GameState.h
@@ -1,18 +1,3 @@
-//
-//  GameState.h
-//  Connect Four Game
-//
-//  Created by Tarek Abdelrahman on 2019-06-07.
-//  Modified by Tarek Abdelrahman on 2020-09-17.
-//  Modified by Salma Emara and Ethan Hugh on 2023-09-06.
-//  Copyright © 2019-2020 Tarek Abdelrahman. All rights reserved.
-//
-//  Permission is hereby granted to use this code in ECE244 at
-//  the University of Toronto. It is prohibited to distribute
-//  this code, either publicly or to third parties.
-//
-//  ECE244 Student: Do NOT modify this file
-
 #ifndef GameState_h
 #define GameState_h
 
